8628975bc7ef6689f9eaa49ba9159d379898a7de
Timothykim21 <timothyakim21@gmail.com>
2018-11-19 11:34:59 -0800

    try it out

---
+++ untitled1/src/sample/Controller.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public TextField textGetName;
+    public TextField textGetQuantity;
+    public TextField textGetCost;
+    public ListView ProductList;
+
+    public void addProduct(ActionEvent actionEvent) {
+        Product temp = new Product()
+    }
+}
